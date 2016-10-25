3fd92230cd5e1e8fd55624c79989f96af5446b00
apple <apple@apples-MacBook-Pro.local>
2016-10-25 15:50:11 +0800

    blogcode

---
+++ www/config_override.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Override configurations.
+'''
+
+__author__ = 'Michael Liao'
+
+configs = {
+    'db': {
+        'user': 'www-data',
+        'password': 'www-data',
+        'db': 'awesome'
+    }
+}
